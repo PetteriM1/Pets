00e94ecc65c76e7b6560ae30d4513584e17e47c3
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-01 23:15:34 +0900

    More Pets (Cow, PolarBear, Sheep) (#3)
    * Create PetSheep
    
    * Create PetCow
    
    * Add Random Sheep Color
    
    randomColor() was made by PetteriM1 (Sheep.java from MobPlugin)
    
    * Make Able to Dye PetSheep
    
    * Create PetPolarBear
    
    * Add Cow, PolarBear, Sheep
    
    * Oops
    
    * Make able to feed Polar Bear
    
    Minecraft Polar Bear doesn't eat fishes, but they were added because it is useless.
    
    * Fix
    
    * Fix

---
+++ src/main/java/me/petterim1/pets/entities/PetPolarBear.java
@@ -0,0 +1,53 @@
+package me.petterim1.pets.entities;
+
+import cn.nukkit.Player;
+import cn.nukkit.entity.passive.EntityPolarBear;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.particle.ItemBreakParticle;
+import cn.nukkit.nbt.tag.CompoundTag;
+import me.petterim1.pets.EntityPet;
+import me.petterim1.pets.Main;
+import me.petterim1.pets.Utils;
+
+public class PetPolarBear extends EntityPet {
+
+    public PetPolarBear(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+
+    @Override
+    public int getNetworkId() {
+        return EntityPolarBear.NETWORK_ID;
+    }
+
+    @Override
+    public float getWidth() {
+        return 1.3f;
+    }
+
+    @Override
+    public float getHeight() {
+        return 1.4f;
+    }
+    
+    @Override
+    public boolean onInteract(Player player, Item item) {
+        switch (player.getInventory().getItemInHand().getId()) {
+            // Minecraft Polar Bear doesn't eat fishes, but they were added because it is useless.
+            case Item.RAW_FISH:
+            case Item.RAW_SALMON:
+                player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
+                this.level.addParticle(new ItemBreakParticle(
+                        this.add(Utils.rand(-0.5, 0.5), this.getMountedYOffset(), Utils.rand(-0.5, 0.5)),
+                        player.getInventory().getItemInHand()));
+
+                this.inLoveTicks = 10;
+                this.setDataFlag(DATA_FLAGS, DATA_FLAG_INLOVE);
+                player.addExperience(Main.getInstance().getPluginConfig().getInt("feedXp"));
+                return true;
+            default:
+                return super.onInteract(player, item);
+        }
+    }
+}
---
+++ src/main/java/me/petterim1/pets/entities/PetSheep.java
@@ -0,0 +1,93 @@
+package me.petterim1.pets.entities;
+
+import cn.nukkit.Player;
+import cn.nukkit.entity.data.ByteEntityData;
+import cn.nukkit.entity.passive.EntitySheep;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemDye;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.level.particle.ItemBreakParticle;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.DyeColor;
+import me.petterim1.pets.EntityPet;
+import me.petterim1.pets.Main;
+import me.petterim1.pets.Utils;
+
+public class PetSheep extends EntityPet {
+    
+    protected int color = 0;
+    
+    public PetSheep(FullChunk chunk, CompoundTag nbt) {
+        super(chunk, nbt);
+    }
+    
+    @Override
+    protected void initEntity() {
+        super.initEntity();
+        
+        this.color = this.namedTag.getByte("Color");
+        this.setDataProperty(new ByteEntityData(DATA_COLOUR, this.color));
+    }
+
+    @Override
+    public int getNetworkId() {
+        return EntitySheep.NETWORK_ID;
+    }
+
+    @Override
+    public float getWidth() {
+        return 0.9f;
+    }
+
+    @Override
+    public float getHeight() {
+        return 1.3f;
+    }
+
+    @Override
+    public boolean onInteract(Player player, Item item) {
+        switch (player.getInventory().getItemInHand().getId()) {
+            case Item.WHEAT:
+                player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
+                this.level.addParticle(new ItemBreakParticle(
+                        this.add(Utils.rand(-0.5, 0.5), this.getMountedYOffset(), Utils.rand(-0.5, 0.5)),
+                        player.getInventory().getItemInHand()));
+
+                this.inLoveTicks = 10;
+                this.setDataFlag(DATA_FLAGS, DATA_FLAG_INLOVE);
+                player.addExperience(Main.getInstance().getPluginConfig().getInt("feedXp"));
+                return true;
+            case Item.DYE:
+                this.color = ((ItemDye) item).getDyeColor().getWoolData();
+                this.saveNBT();
+                return true;
+            default:
+                return false;
+        }
+    }
+    
+    @Override
+    public void setRandomType() {
+        this.color = randomColor();
+        this.saveNBT();
+    }
+    
+    @Override
+    public void saveNBT() {
+        super.saveNBT();
+        this.color = this.namedTag.getByte("Color");
+        this.setDataProperty(new ByteEntityData(DATA_COLOUR, this.color));
+    }
+    
+    // randomColor() was made by PetteriM1 (Sheep.java from MobPlugin)
+    private int randomColor() {
+        int rand = Utils.rand(0, 2500);
+
+        if (rand < 125 && 0 <= rand) return DyeColor.BLACK.getDyeData();
+        else if (rand < 250 && 125 <= rand) return DyeColor.GRAY.getDyeData();
+        else if (rand < 375 && 250 <= rand) return DyeColor.LIGHT_GRAY.getDyeData();
+        else if (rand < 500 && 375 <= rand) return DyeColor.GRAY.getDyeData();
+        else if (rand < 541 && 500 <= rand) return DyeColor.PINK.getDyeData();
+        else return DyeColor.WHITE.getDyeData();
+    }
+}
