a3c3970aedcee501060df29bb5c5a18aa0454ebe
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-08-01 16:27:32 +0900

    Make able to feed Polar Bear
    Minecraft Polar Bear doesn't eat fishes, but they were added because it is useless.

--- src/main/java/me/petterim1/pets/entities/PetPolarBear.java
+++ src/main/java/me/petterim1/pets/entities/PetPolarBear.java
@@ -27,4 +27,24 @@ public class PetPolarBear extends EntityPet {
     public float getHeight() {
         return 1.4f;
     }
+    
+    @Override
+    public boolean onInteract(Player player, Item item) {
+        switch (player.getInventory().getItemInHand().getId()) {
+            // Minecraft Polar Bear doesn't eat fishes, but they were added because it is useless.
+            case Item.RAW_FISH:
+            case Item.RAW_SALMON:
+                player.getInventory().decreaseCount(player.getInventory().getHeldItemIndex());
+                this.level.addParticle(new ItemBreakParticle(
+                        this.add(Utils.rand(-0.5, 0.5), this.getMountedYOffset(), Utils.rand(-0.5, 0.5)),
+                        player.getInventory().getItemInHand()));
+
+                this.inLoveTicks = 10;
+                this.setDataFlag(DATA_FLAGS, DATA_FLAG_INLOVE);
+                player.addExperience(Main.getInstance().getPluginConfig().getInt("feedXp"));
+                return true;
+            default:
+                return super.onInteract(player, item);
+        }
+    }
 }
